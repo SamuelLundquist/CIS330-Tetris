89706d85b8d33107a0e63b0b91821e3ff77792f7
Samzillard <sglundquist7@gmail.com>
2019-05-09 18:03:24 -0700

    added title window

--- definitions.h
+++ definitions.h
@@ -3,23 +3,22 @@
 
 const int gameWin_height = 35;
 const int gameWin_width = 50;
-const int gameWin_x = 4;
+const int gameWin_x = 2;
 const int gameWin_y = 1;
 
-const int scoreWin_height = 20;
+const int scoreWin_height = 25;
 const int scoreWin_width = 20;
-const int scoreWin_x = 58;
-const int scoreWin_y = 16;
+const int scoreWin_x = 54;
+const int scoreWin_y = 11;
 
 const int hintWin_height = 10;
 const int hintWin_width = 20;
-const int hintWin_x = 58;
+const int hintWin_x = 54;
 const int hintWin_y = 1;
 
+const int titleWin_height = 35;
+const int titleWin_width = 20;
+const int titleWin_x = 76;
+const int titleWin_y = 1;
+
 #endif
-
-struct block{
-
-
-
-} BLOCK;
--- game.cc
+++ game.cc
@@ -16,28 +16,75 @@ using namespace std;
 char** block_data;
 
 //Define all windows
-WINDOW * gameWin, *scoreWin, *hintWin;
+WINDOW * gameWin, *scoreWin, *hintWin, *titleWin;
 
 
 void windowsInit()
 {
-   gameWin = newwin(gameWin_height, gameWin_width, gameWin_y, gameWin_x);
-   scoreWin = newwin(scoreWin_height, scoreWin_width, scoreWin_y, scoreWin_x);
-   hintWin = newwin(hintWin_height, hintWin_width, hintWin_y, hintWin_x);
+    gameWin = newwin(gameWin_height, gameWin_width, gameWin_y, gameWin_x);
+    scoreWin = newwin(scoreWin_height, scoreWin_width, scoreWin_y, scoreWin_x);
+    hintWin = newwin(hintWin_height, hintWin_width, hintWin_y, hintWin_x);
+    titleWin = newwin(titleWin_height, titleWin_width, titleWin_y, titleWin_x);
+    refresh();
 
-   box(gameWin,0,0);
-   box(scoreWin,0,0);
-   box(hintWin,0,0);
-   mvwprintw(scoreWin, 2, 5, "T E T R I S");
-   mvwprintw(hintWin, 1, 3, "hint window!");
-   refresh();
-   
-   wrefresh(gameWin);
-   wrefresh(scoreWin);
-   wrefresh(hintWin);
-   
+    //Init Box Win
+    box(gameWin,0,0);
+    wrefresh(gameWin);
 
+    //Init Hint Win
+    box(hintWin,0,0);
+    mvwprintw(hintWin, 0, 4, " NEXT BLOCK ");
+    wrefresh(hintWin);  
+
+    //Init Title Win
+    mvwprintw(titleWin, 1, 0,  " __________");
+    mvwprintw(titleWin, 2, 0,  "|___    ___|");
+    mvwprintw(titleWin, 3, 0,  "    |  |");
+    mvwprintw(titleWin, 4, 0,  "    |  |");
+    mvwprintw(titleWin, 5, 0,  "    |__|");
+    mvwprintw(titleWin, 6, 0,  " __________");
+    mvwprintw(titleWin, 7, 0,  "|    ______|");
+    mvwprintw(titleWin, 8, 0,  "|  |___");
+    mvwprintw(titleWin, 9, 0,  "|   ___|");
+    mvwprintw(titleWin, 10, 0, "|  |_______");
+    mvwprintw(titleWin, 11, 0, "|__________|");
+    mvwprintw(titleWin, 12, 0,  " __________");
+    mvwprintw(titleWin, 13, 0,  "|___    ___|");
+    mvwprintw(titleWin, 14, 0,  "    |  |");
+    mvwprintw(titleWin, 15, 0,  "    |  |");
+    mvwprintw(titleWin, 16, 0,  "    |__|");
+    mvwprintw(titleWin, 17, 0, " ________");
+    mvwprintw(titleWin, 18, 0, "|   ___  \\");
+    mvwprintw(titleWin, 19, 0, "|  |___)  |");
+    mvwprintw(titleWin, 20, 0, "|   __   /");
+    mvwprintw(titleWin, 21, 0, "|  |  \\  \\");
+    mvwprintw(titleWin, 22, 0, "|__|   \\__\\");
+    mvwprintw(titleWin, 23, 0, " __________");
+    mvwprintw(titleWin, 24, 0, "|___    ___|");
+    mvwprintw(titleWin, 25, 0, "    |  |");
+    mvwprintw(titleWin, 26, 0, " ___|  |___");
+    mvwprintw(titleWin, 27, 0, "|__________|");
+    mvwprintw(titleWin, 28, 0, " __________");
+    mvwprintw(titleWin, 29, 0, "/   _______|");
+    mvwprintw(titleWin, 30, 0, "|  (_______");
+    mvwprintw(titleWin, 31, 0, "\\_______   \\");
+    mvwprintw(titleWin, 32, 0, " _______)  |");
+    mvwprintw(titleWin, 33, 0, "|__________/");
+    wrefresh(titleWin);
+
+    //Init Score Win
+    box(scoreWin,0,0);
+    mvwprintw(scoreWin, 19, 5, "Controls:");
+    mvwprintw(scoreWin, 20, 2, "Left: left arr");
+    mvwprintw(scoreWin, 21, 2, "Right: right arr");
+    mvwprintw(scoreWin, 22, 2, "Drop: down arr");
+    mvwprintw(scoreWin, 23, 2, "Rotate: up arr");
+    wrefresh(scoreWin);
+
+    refresh();
 }
+
+
 
 int main()
 {
