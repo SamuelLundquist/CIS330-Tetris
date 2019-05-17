d43478cf5ca7e089321c52196ba1944155e41247
Matthew <mstrubl2@uoregon.edu>
2019-05-17 04:52:37 -0700

    added rotate function

--- game.cc
+++ game.cc
@@ -198,6 +198,8 @@ void initWindows()
     wrefresh(controlsWin);
 }
 
+//given piece number (int n), assign piece struct values and update
+//block data matrix with piece 
 void makePiece(int n) 
 {
 	piece.origin[0] = pieces[n][1];
@@ -212,6 +214,7 @@ void makePiece(int n)
 	}
 }
 
+//remove horizontal line y
 void rmLine(int y) 
 {
 	for(int x = 0; x < blockWin_width/2; x++) 
@@ -219,6 +222,92 @@ void rmLine(int y)
 		block_data[y][x] = 0;
 	}
 }
+
+//rotate piece if space available
+//positive input = clockwise, negative = counterclockwise
+//
+//create a "sub-graph" with respect to the origin of piece
+//the x coordinate of each block = x - originx
+//since y is inverted (starts at 0 from the top)
+//y = originy - y
+//clockwise rotation is: x = y, y = -x
+//therefore, plug in above values of x to this equation
+//then, check if space is open
+//if so, update block data and piece data
+void rotate(int n) 
+{
+	if(!n) //n must have value
+	{
+		return;
+	}
+	unsigned int originx = piece.origin[0];
+	unsigned int originy = piece.origin[1];
+
+	unsigned int color = block_data[originy][originx];
+
+	if(color == 7) //don't rotate squares
+	{
+		return;
+	}
+
+	int* newloc = (int*)malloc(sizeof(int)*2*piece_size);
+
+
+	//clear the original blocks from block data
+	for(int i = piece_size; i--;) 
+	{
+		block_data[piece.blocks[i][1]][piece.blocks[i][0]] = 0;
+	}
+
+
+	for(int i = piece_size; i--;) 
+	{
+		int oldx = piece.blocks[i][0] - originx;
+		int oldy = originy - piece.blocks[i][1];
+	
+		int newx = (n/abs(n))*oldy;
+		int newy = (n/abs(n))*-oldx;
+		
+		int nlx = newloc[2*i] = piece.blocks[i][0] + (newx - oldx);
+		int nly = newloc[2*i+1] = piece.blocks[i][1] - (newy - oldy);
+		printf("co(%d,%d)o(%d,%d)n(%d,%d),cn(%d,%d)",
+				piece.blocks[i][0] ,piece.blocks[i][1], oldx, oldy,
+		     		newx, newy, nlx, nly);
+		printf("%d",blockWin_width);
+		fflush(stdout);
+		//if there's a block there, or if the block is
+		//outside of the window, stop the function
+		if(block_data[nly][nly] || nlx < 0 || nly < 0 
+						|| nlx >= blockWin_width/2 || nly >= blockWin_height/2) 
+			
+		{
+			//reconstruct the piece
+			for(int i = piece_size; i--;) 
+			{
+				block_data[piece.blocks[i][1]][piece.blocks[i][0]] = color;
+			}
+			free(newloc);
+			return;
+		}
+	}
+
+
+	
+	//update the block data and piece data
+	for(int i = piece_size; i--;) 
+	{
+		block_data[newloc[2*i+1]][newloc[2*i]] = color;
+		
+		//update blocks in piece, origin should stay the same
+		piece.blocks[i][0] = newloc[2*i];
+		piece.blocks[i][1] = newloc[2*i+1];
+			
+	}	
+
+	free(newloc);
+}
+
+
 int main()
 {
    initscr();
@@ -262,8 +351,27 @@ int main()
       makePiece(i);
 	  updateBlockWindow();
 	  getch();
+	  rotate(1);
+	  updateBlockWindow();
+	  getch();
+	  rotate(1);
+	  updateBlockWindow();
+	  getch();
+	  rotate(1);
+	  updateBlockWindow();
+	  getch();
+	  rotate(1);
+	  updateBlockWindow();
+	  getch();
+	  rotate(-1);
+	  updateBlockWindow();
+	  getch();
+	  rmLine(2);
+	  rmLine(3);
 	  rmLine(4);
 	  rmLine(5);
+	  rmLine(6);
+	  rmLine(7);
    }
 
    freePieceData();
