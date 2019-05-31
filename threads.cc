bd4e6e29481c76fd022e16044f7b8136363a1802
Matthew <mstrubl2@uoregon.edu>
2019-05-30 19:16:55 -0700

    added dropFunc and inputFunc thread skeletons

---
+++ threads.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <time.h>
+#include "definitions.h"
+
+using namespace std;
+
+#define DROP 0
+#define MOVE_L 1
+#define MOVE_R 2
+#define CCW_ROT 3
+#define CW_ROT 4
+#define DROP_F 5
+
+
+//moveQueue[5] - queue of moves to be executed by main thread
+
+
+
+//adds DROP to the moveQueue every period of time 
+//dictated by the current falling speed
+void dropFunc()
+{
+	
+}
+
+//adds corresponding input to the moveQueue based on user inputted
+//characters to be executed by the main
+void inputFunc() 
+{
+
+}
